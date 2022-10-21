a6519409786bb306472a7314557721f95ae5bb27
Naomi Pham <75587231+naomi-pham@users.noreply.github.com>
2022-10-22 04:32:23 +0700

    Update Nav.js

--- daily-captures/components/Nav.js
+++ daily-captures/components/Nav.js
@@ -5,7 +5,7 @@ export default function Nav() {
         <nav className='flex justify-end items-baseline gap-5'>
             <input type="text" placeholder='Search...' className='text-sm px-4 py-2 sm:w-48 w-full bg-zinc-800 border border-zinc-600 rounded-lg' />
             <div className="flex">           
-                <p className='hover:bg-zinc-700 text-lg rounded-lg py-2 px-3 font-semibold'>
+                <p className='hover:bg-zinc-800 text-emerald-500 text-lg rounded-lg py-2 px-3'>
                     <Link href={"/"}>Home</Link>
                 </p>
                 <p className='hover:bg-zinc-800 text-lg rounded-lg py-2 px-3'>
