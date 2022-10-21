ea7bfc35ec602ec93821dbccca8ec444a15a91e6
Naomi Pham <phambaonguyendn@gmail.com>
2022-10-21 20:04:02 +0200

    Add key to gallery component

--- daily-captures/pages/gallery.js
+++ daily-captures/pages/gallery.js
@@ -45,27 +45,15 @@ export default function Gallery({posts}) {
         <div>
           <h1 className='text-4xl sm:text-5xl lg:text-6xl mt-4 lg:mt-0'>Gallery</h1>
           <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-8 items-start">
-<<<<<<< HEAD
             {posts.map(post => (
-              <div className='bg-zinc-700 overflow-hidden'>
+              <div key={post.id} className='bg-zinc-700 overflow-hidden'>
                   <Link href={"/posts/" + post.slug}>
                     <Image className='hover:scale-125 hover:cursor-pointer object-fit transition-transform ease-in-out duration-300' width={300} height={210} src={post.coverPhoto.url} />
                   </Link>
                   <p className='pl-2 pt-2 opacity-95 
                   '>{post.title} | <span className='opacity-60'>{post.datePublished}</span></p>
               </div>
-            ))}
-=======
-          {posts.map(post => (
-                <div key={post.id} className='bg-zinc-800 overflow-hidden'>
-                    <Link href={"/posts/" + post.slug}>
-                        <img className='w-full hover:scale-125 hover:cursor-pointer' src={post.coverPhoto.url} />
-                    </Link>
-                    <p className='pl-2 pt-3 opacity-95 bg-zinc-800
-                    '>{post.title} | <span className='opacity-60'>{post.datePublished}</span></p>
-                </div>
-          ))}
->>>>>>> 88c496fb1c266e20a91935ce69586df981c2249c
+            ))}  
            </div>
         </div>    
       </main>
