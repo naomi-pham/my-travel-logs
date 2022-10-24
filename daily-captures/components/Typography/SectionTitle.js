d6767c7bff34a171b1144542e3fa74b7c9d1259d
Naomi Pham <75587231+naomi-pham@users.noreply.github.com>
2022-10-24 14:24:31 +0200

    Add files via upload

---
+++ daily-captures/components/Typography/SectionTitle.js
@@ -0,0 +1,9 @@
+const SectionTitle = ({children}) => {
+    return (
+    <h2 className='text-4xl sm:text-5xl lg:text-6xl mt-4 lg:mt-0'>
+          {children}
+    </h2>
+    )
+  }
+  
+  export default SectionTitle
