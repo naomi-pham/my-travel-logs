af233b51995c3d0ac997536102f634aedcf732d2
Naomi Pham <75587231+naomi-pham@users.noreply.github.com>
2022-10-25 18:47:10 +0200

    Add files via upload

--- daily-captures/components/Nav.js
+++ daily-captures/components/Nav.js
@@ -40,7 +40,7 @@ export default function Nav() {
             <nav className='flex items-baseline gap-5'>
                 <input type="text" placeholder="Search..." className='sm:block hidden text-sm px-4 py-2 sm:w-56 w-full bg-zinc-900 border border-1 border-zinc-600' />
                 <i  onClick={openNav} 
-                    className={`right-8 bx ${isActive ? "bx-menu" : "bx-x"} sm:hidden z-40 text-3xl`}
+                    className={`absolute right-8 bx ${isActive ? "bx-menu" : "bx-x"} sm:hidden z-40 text-3xl`}
                 >
                 </i>
                 
@@ -60,20 +60,24 @@ export default function Nav() {
 
             {/* nav-mobile */}
 
-            <nav className={`${isActive ? "hidden" : ""} sm:hidden fixed w-full left-0 top-0 bottom-0 opacity-90 z-10 p-8 bg-zinc-900`}>
-                <div className="flex flex-col">
-                    <input type="text" placeholder="Search..." className='mt-12 my-6 text-sm px-4 py-2 w-9/12 bg-zinc-900 border border-1 border-zinc-600' />              
+            <nav className={`${isActive ? "-translate-x-full" : "-translate-x-0"} duration-700 transition-transform sm:hidden fixed w-full left-0 top-0 bottom-0 opacity-90 z-10 p-8 bg-zinc-900`}>
+                <div className="flex flex-col fixed bottom-8 pl-2">
+                    <input type="text" placeholder="Search..." className='mt-12 my-6 text-lg pl-2 py-2 bg-zinc-800 w-full' />              
                     {NavItems.map((item, index) => {
                         return (
-                            <p key={index} className  ={`text-xl rounded-lg py-2 px-3 font-semibold
-                                    ${router.pathname == `${item.path}` ? "underline-emerald opacity-90" : ""} hover:opacity-80`}
-                            >
-                                <NavLink href={`${item.path}`}>
-                                    <a onClick={() => setIsActive(true)}>
-                                        {item.name}
-                                    </a>
-                                </NavLink>
-                            </p>
+                            <div>
+                                <hr className="bg-zinc-900 border-[0.01]" />
+                                <p key={index} className  ={`text-xl rounded-lg py-2 pl-1
+                                        ${router.pathname == `${item.path}` ? "opacity-90" : ""} hover:opacity-80`}
+                                >
+                                    <NavLink href={`${item.path}`}>
+                                        <a onClick={() => setIsActive(true)}>
+                                            {item.name}
+                                        </a>
+                                    </NavLink>
+                                </p>
+                                <hr className="bg-zinc-900 border-[0.01]" />
+                            </div>
                         )
                     })}                   
                 </div>
